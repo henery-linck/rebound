0869519f46432703d774f7a519208a0a1539919a
Henery Linck Rosenhaim <henery@trinca.com>
2026-04-22 14:13:55 -0300

    Adding the ball to the game.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 direction;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        direction = new Vector2(-1, Random.Range(-0.7f, 0.7f)).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = direction * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            float hitOffset = transform.position.y - collision.transform.position.y;
+
+            direction = new Vector2(
+                -direction.x,
+                direction.y + hitOffset * 0.5f
+            ).normalized;
+        }
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            direction = new Vector2(direction.x, -direction.y).normalized;
+        }
+    }
+}
